1637faf2ad8b95c8b1cca9ba6c5ea64e055ee8ba
Joe <joseph.m.napoli@vanderbilt.edu>
2023-04-12 14:45:43 -0500

    Added amplify stuff

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "swingersclub": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
